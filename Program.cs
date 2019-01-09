5e61ceb0230648d1ec2e3dab54a78c37d25e3b10
Griffin Tucker <gtucker96@gmail.com>
2019-01-09 18:31:00 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3610Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
